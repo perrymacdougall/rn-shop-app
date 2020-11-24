b6bf6e770d38604cd8c808072575b17c97403df8
perrymacdougall <perrymacdougall@gmail.com>
2020-11-24 18:59:17 -0500

    configured React-Navigation v5 and built simple stack navigator

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -5,18 +5,11 @@ import { useSelector } from 'react-redux';
 const ProductsOverviewScreen = () => {
   const products = useSelector((state) => state.products.availableProducts);
   return (
-    <div>
-      <FlatList
-        data={products}
-        keyExtractor={(item) => item.id}
-        renderItem={(itemData) => <Text>{itemData.item.title}</Text>}
-      />
-      <FlatList
-        data={products}
-        keyExtractor={(item) => item.id}
-        renderItem={(itemData) => <Text>{itemData.item.title}</Text>}
-      />
-    </div>
+    <FlatList
+      data={products}
+      keyExtractor={(item) => item.id}
+      renderItem={(itemData) => <Text>{itemData.item.title}</Text>}
+    />
   );
 };
 
