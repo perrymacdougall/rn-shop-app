d084901275ff1b25986e0bb1a6dabcfd78a2e656
perrymacdougall <perrymacdougall@gmail.com>
2020-11-24 14:44:47 -0500

    configured eslint, prettier and the Airbnb style guide for this project

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -2,15 +2,22 @@ import React from 'react';
 import { FlatList, Text } from 'react-native';
 import { useSelector } from 'react-redux';
 
-const ProductsOverviewScreen = props => {
-    const products = useSelector(state => state.products.availableProducts);
-    return (
-        <FlatList
-            data={products}
-            keyExtractor={item => item.id}
-            renderItem={itemData => <Text>{itemData.item.title}</Text>}
-        />
-    );
+const ProductsOverviewScreen = () => {
+  const products = useSelector((state) => state.products.availableProducts);
+  return (
+    <div>
+      <FlatList
+        data={products}
+        keyExtractor={(item) => item.id}
+        renderItem={(itemData) => <Text>{itemData.item.title}</Text>}
+      />
+      <FlatList
+        data={products}
+        keyExtractor={(item) => item.id}
+        renderItem={(itemData) => <Text>{itemData.item.title}</Text>}
+      />
+    </div>
+  );
 };
 
 export default ProductsOverviewScreen;
